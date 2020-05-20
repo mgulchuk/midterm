cd7d77593d7586da7628765f6707d2e79dff2744
Michael Gulchuk <mgulchuk@mail.greenriver.edu>
2020-05-20 18:37:12 -0400

    added summary page

--- index.php
+++ index.php
@@ -8,9 +8,11 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+session_start();
 
 // Require the autoload file
 require_once('vendor/autoload.php');
+require_once("model/data-layer.php");
 
 
 // Instantiate the F3 Base class
@@ -23,11 +25,34 @@ $f3->route('GET /', function()
     echo $view -> render("views/home.html");
 });
 
-$f3->route('GET|POST /survey', function()
+$f3->route('GET|POST /survey', function($f3)
 {
+    $survey = getSurvey();
+
+    //If the form has been submitted
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        //Store the data in the session array
+        $_SESSION['name'] = $_POST['name'];
+        $_SESSION['survey'] = $_POST['survey'];
+
+        //Redirect to summary page
+        $f3->reroute('summary');
+        session_destroy();
+    }
+
+    $f3->set('survey', $survey);
+
     $view = new Template();
     echo $view -> render("views/survey.html");
 });
+
+$f3->route('GET /summary', function()
+{
+    $view = new Template();
+    echo $view -> render("views/summary.html");
+});
+
 
 // Run fat free
 $f3->run();
